b55564a3e567d8dc937ce706a592eb60e9b72578
Jose Trinidad <internick18@gmail.com>
2019-09-30 15:52:57 -0500

    first Commit.. add permission with dexter

---
+++ app/src/main/java/android/galosoft/com/placeproject/PermissionActivity.java
@@ -0,0 +1,80 @@
+package android.galosoft.com.placeproject;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class PermissionActivity extends AppCompatActivity {
+
+    private Button btnGrant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission);
+
+        btnGrant = findViewById(R.id.btn_grant);
+
+        if(ContextCompat.checkSelfPermission(PermissionActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            finish();
+            return;
+        }
+
+
+        btnGrant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(PermissionActivity.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                startActivity(new Intent(PermissionActivity.this, MapActivity.class));
+                                finish();
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+                                if(response.isPermanentlyDenied()){
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(PermissionActivity.this);
+                                    builder.setTitle("Permission denied")
+                                            .setMessage("Permission to access is permanent denied... please check settings")
+                                            .setNegativeButton("Cancek", null)
+                                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent();
+                                                    intent.setAction(Settings.ACTION_APPLICATION_SETTINGS);
+                                                    intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                                }
+                                            }).show();
+                                } else {
+                                    Toast.makeText(PermissionActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+    }
+}
